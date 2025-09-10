b219a3e642b6c55bc848df256d424eaef3bb0eea
TorbjornBak <s203555@student.dtu.dk>
2025-09-10 15:54:58 +0200

    Updated to cuda

--- scripts/dnn.py
+++ scripts/dnn.py
@@ -30,6 +30,18 @@ y_filtered = y[mask]
 
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+
+
+
+if torch.cuda.is_available(): 
+    device = torch.device("cuda")
+
+elif torch.backends.mps.is_available(): 
+    device = torch.device("mps")
+else: 
+    device = torch.device("cpu")
+
+
 print(f"Using {device} device")
 
 # Split filtered data into train and test sets (1/5 for test)
