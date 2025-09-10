dbc5786659c585f23cfd283fe77aa47664e9ae7c
TorbjornBak <s203555@student.dtu.dk>
2025-09-10 13:47:57 +0200

    Added PCA, updated kmer sampling

--- scripts/kmer_sampling.py
+++ scripts/kmer_sampling.py
@@ -1,21 +1,17 @@
 import mmap
-import sys
+import sys, os
 import numpy as np
+import pandas as pd
 # Reads one fasta file
 
 # One-hot encoding.
-# Loop over file, once kmer has been found, don't look for it again.
-
-# Load to numpy array, size should be calculated based on total possible nr
-# of k-mers for a given kmer size
-
-
 
 def read_fasta_binary(file_path):
 
 	sequences = list()
 
 	with open(file_path, 'r+b') as f:
+		
 		mm = mmap.mmap(f.fileno(),0, access=mmap.ACCESS_READ)
 	
 		header_position = 0
@@ -34,7 +30,7 @@ def read_fasta_binary(file_path):
 			seq_start = header_end + 1
 			
 
-			header = mm.read(header_end - header_position)
+			#header = mm.read(header_end - header_position)
 		
 			
 			
@@ -44,9 +40,6 @@ def read_fasta_binary(file_path):
 
 			current_sequence = mm.read(seq_end - seq_start)
 			sequences.append(current_sequence)
-
-
-
 
 	mm.close()		
 
@@ -59,7 +52,7 @@ def kmerize_sequences_prefix_filtering(sequences, kmer_prefix, kmer_suffix_size,
 	array = np.zeros(array_size, dtype = int)
 	
 	kmer_prefix_size = len(kmer_prefix)
-	found_count = 0
+	kmer_count = 0
 
 	for sequence in sequences:
 		sequence = sequence.replace(b"\n", b"")
@@ -67,26 +60,24 @@ def kmerize_sequences_prefix_filtering(sequences, kmer_prefix, kmer_suffix_size,
 		sequence = sequence.translate(table)
 		
 		
-		# fill out numpy 
+		# Finds kmers and one-hot-encode them
 		current_kmer_prefix_location = sequence.find(kmer_prefix)
 
 		while current_kmer_prefix_location != -1:
 
-			found_count += 1
+			kmer_count += 1
 			kmer_suffix_start_location = current_kmer_prefix_location + kmer_prefix_size
 			
 			kmer_suffix = sequence[kmer_suffix_start_location : kmer_suffix_start_location + kmer_suffix_size]
 			
 			if b'n' not in kmer_suffix:
+				# Converts dna to binary to use for indexing np.array
 				kmer_suffix_binary = dna_to_binary(kmer_suffix,	kmer_suffix_size)
-
-				#print(f"{kmer_suffix=}")
-
 
 				array[kmer_suffix_binary] = 1
 
 			current_kmer_prefix_location = sequence.find(kmer_prefix, current_kmer_prefix_location + kmer_prefix_size)
-	print(f'Total kmers: {found_count}')
+	print(f'Total kmers: {kmer_count}')
 	return array
 
 def dna_to_binary(dna, kmer_size):
@@ -146,17 +137,19 @@ def bin_to_dna(number, kmer_size):
 
 			
 def get_array_size(alphabet_size, kmer_size):
+	# Load to numpy array, size is calculated based on total possible nr
+	# of k-mers for a given kmer size
 	return alphabet_size ** kmer_size
 
 
 
-def test_kmer_sampler(iterations = 1000, filepath = "data/test/511145.fna"):
+def test_kmer_sampler(iterations = 1000, file_path = "data/test/511145.fna"):
 	kmer_prefix = b"CGTGATGT"
 	kmer_suffix_size = 8
 	vector = list()
 	for iteration in range(iterations):
 		print(f'Iteration nr: {iteration}')
-		sequences = read_fasta_binary(file_path="data/test/511145.fna")
+		sequences = read_fasta_binary(file_path=file_path)
 		array_size = get_array_size(alphabet_size = 4, kmer_size = kmer_suffix_size)
 		array = kmerize_sequences_prefix_filtering(sequences, kmer_prefix, kmer_suffix_size, array_size)
 
@@ -164,23 +157,62 @@ def test_kmer_sampler(iterations = 1000, filepath = "data/test/511145.fna"):
 		vector.append(array)
 
 
-def kmer_sampling_multiple_files(directory, genome_ids):
-	kmer_prefix = b"CGTGATGT"
-	kmer_suffix_size = 8
-	vector = list()
+def kmer_sampling_multiple_files(directory, genome_ids = None, file_names = None, labels = None, prefix = ".fna"):
+	kmer_prefix = b"CGTGAT"
+	kmer_suffix_size = 11
+	arrays = list()
+	y_labels = list()
+	array_size = get_array_size(alphabet_size = 4, kmer_size = kmer_suffix_size)
 	
-	for _ in range(100):
+	if genome_ids is not None:
+		file_names = [f'{genome_id}{prefix}' for genome_id in genome_ids]
 
-		for id in genome_ids:
 
-			file = f'{directory}/{id}.fna'
-			sequences = read_fasta_binary(file_path=file)
-			array_size = get_array_size(alphabet_size = 4, kmer_size = kmer_suffix_size)
-			array = kmerize_sequences_prefix_filtering(sequences, kmer_prefix, kmer_suffix_size, array_size)
+	for file_name in file_names:
+		file_path = f'{directory}/{file_name}'
+		sequences = read_fasta_binary(file_path=file_path)
+		
+		array = kmerize_sequences_prefix_filtering(sequences, kmer_prefix, kmer_suffix_size, array_size)
 
-			print(f'Unique kmers: {array.sum()}')
-			vector.append(array)
+		print(f'Unique kmers: {array.sum()}')
+		arrays.append(array)
 
+		genome_id = file_name.strip(prefix)
+
+		y_labels.append(labels[genome_id])
+
+	
+	return arrays, y_labels
+	
+
+def load_labels(file_path = "downloads/genome_lineage", id = "genome_id", label = "class"):
+
+	genome_lineage = pd.read_csv(file_path, sep ="\t")
+
+	genome_lineage = genome_lineage.dropna(subset = [id, label])
+
+	labels = dict(zip(genome_lineage[id].apply(str), genome_lineage[label]))
+	return labels
+
+
+def find_files_to_kmerize(directory, prefix = ".fna", id = "genome_id", label = "class"):
+
+	dir_list = os.listdir(directory)
+
+
+
+	dir_list = [file for file in dir_list if prefix in file]
+
+
+	labels = load_labels(id = id, label = label)
+
+	
+
+	final_dir_list = [dir for dir in dir_list if dir.replace(prefix, "") in labels]
+
+
+	return final_dir_list, labels
+	
 
 if __name__ == "__main__":
 	# kmer_prefix = b"CGTGA"
@@ -189,14 +221,24 @@ if __name__ == "__main__":
 	# array_size = get_array_size(alphabet_size = 4, kmer_size = kmer_suffix_size)
 	# array = kmerize_sequences_prefix_filtering(sequences, kmer_prefix, kmer_suffix_size, array_size)
 	# print(f'Unique kmers: {array.sum()}')
-	#test_kmer_sampler()
-	kmer_sampling_multiple_files(directory = "data", genome_ids=[469009.4,
- 1309411.5,
- 1123738.3,
- 551115.6,
- 1856298.3,
- 1706000.3,
- 28901.2925,
- 28901.2926,
- 28901.2927,
- 28901.2928])
+	# test_kmer_sampler()
+	# kmer_sampling_multiple_files(directory = "data", 
+	# 						  genome_ids=[
+	# 							  		469009.4,
+	# 									1309411.5,
+	# 									1123738.3,
+	# 									551115.6,
+	# 									1856298.3,
+	# 									1706000.3,
+	# 									28901.2925,
+	# 									28901.2926,
+	# 									28901.2927,
+	# 									28901.2928])
+	file_names, labels = find_files_to_kmerize(directory="data", prefix = ".fna")
+	#labels = load_labels(file_path="downloads/genome_lineage")
+	
+	X, y = kmer_sampling_multiple_files(directory="data", file_names=file_names, labels = labels)
+	print(f'{len(file_names)=}')
+	print(len(y))
+	print(f'{y=}')
+
---
+++ scripts/pca_analysis.py
@@ -0,0 +1,42 @@
+from kmer_sampling import find_files_to_kmerize, kmer_sampling_multiple_files, load_labels
+
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+
+
+
+if __name__ == "__main__":
+	file_names, labels = find_files_to_kmerize(directory="data", prefix = ".fna")
+	#labels = load_labels(file_path="downloads/genome_lineage")
+	
+	X, y = kmer_sampling_multiple_files(directory="data", file_names=file_names, labels = labels)
+	#print()
+
+	X_np = np.stack(X, axis=0)
+
+	pca = PCA(n_components=2, random_state=0)
+	X_pcs = pca.fit_transform(X_np)
+
+	print(pca.explained_variance_ratio_)
+	
+	labels = np.unique(y)
+
+	label2id = {label: i for i, label in enumerate(labels) }
+	print(label2id)
+	print(y)
+	color_list = [label2id[l] for l in y]
+	
+
+	plt.figure(figsize=(6,5))
+	
+	plt.scatter(X_pcs[:, 0], X_pcs[:, 1], c=color_list, cmap='coolwarm', edgecolor='k')
+	plt.xlabel(f'PC1')
+	plt.ylabel(f'PC2')
+	plt.title('PCA projection')
+	plt.legend(title='Label', frameon=False)
+	plt.tight_layout()
+	plt.savefig('results/pca_analysis.jpg')
